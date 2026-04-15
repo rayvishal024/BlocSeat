e2ec5484a57d523728a6a84b8882583a1a09edea
rayvishal <rayvishal2005@gmail.com>
2026-04-15 08:57:47 +0530

    set static path for render

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.routes.js";
 import seatRoutes from "./routes/seat.routes.js";
@@ -9,17 +11,21 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // middleware 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // routes 
 app.use("/api/auth", authRoutes);
 app.use("/api/seats", seatRoutes);
 
-// test routes
+// default routes
+app.get("/", (req, res) => {
+     res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 app.listen(port, () => {
      console.log(`Server running on ${port}`);
